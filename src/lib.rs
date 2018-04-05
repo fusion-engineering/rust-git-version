06a383773cf87ba89d1e1a56d2413af6a5fab694
Guillaume P <texitoi@texitoi.eu>
2018-04-05 11:07:55 +0200

    Add the `set_env_with_name` function for env with custom name

--- src/lib.rs
+++ src/lib.rs
@@ -20,9 +20,22 @@ use std::process::Command;
 /// Also instructs cargo to *always* re-run the build script and recompile the
 /// code, to make sure the version number is always correct.
 pub fn set_env() {
+	set_env_with_name("VERSION");
+}
+
+/// Same as `set_env`, but using `name` as environment variable.
+///
+/// You can, for example, override the `CARGO_PKG_VERSION` using in
+/// your `build.rs` script:
+///
+/// ```
+/// extern crate git_version;
+/// fn main() { git_version::set_env_with_name("CARGO_PKG_VERSION"); }
+/// ```
+pub fn set_env_with_name(name: &str) {
 	let cmd = Command::new("git").args(&["describe", "--always", "--dirty=-modified"]).output().unwrap();
 	assert!(cmd.status.success());
 	let ver = std::str::from_utf8(&cmd.stdout[..]).unwrap().trim();
-	println!("cargo:rustc-env=VERSION={}", ver);
+	println!("cargo:rustc-env={}={}", name, ver);
 	println!("cargo:rerun-if-changed=(nonexistentfile)");
 }
