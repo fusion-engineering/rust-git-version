46c998b88b696c8c14211fc6a03ca27657162077
Maarten de Vries <maarten@de-vri.es>
2019-03-24 17:29:03 +0100

    Use syn to parse macro input, and delegate to impl crate.

--- git-version/tests/version.rs
+++ git-version/tests/version.rs
@@ -8,6 +8,6 @@ fn git_describe_is_right() {
 		.expect("failed to execute git").stdout;
 	let name = std::str::from_utf8(&vec[..vec.len()-1]).expect("non-utf8 error?!");
 	println!("name = {}", name);
-	println!("GIT_VERSION = {}", git_describe!(--dirty=-modified));
-	assert_eq!(git_describe!(--dirty=-modified), name);
+	println!("GIT_VERSION = {}", git_describe!("--always", "--dirty=-modified"));
+	assert_eq!(git_describe!("--always", "--dirty=-modified"), name);
 }
