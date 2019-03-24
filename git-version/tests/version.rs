1b49dbeb948f2afc7a30fce2f78dbd6e86b46397
Maarten de Vries <maarten@de-vri.es>
2019-03-24 17:27:27 +0100

    Re-indent files with tabs and delete backup file.

--- git-version/tests/version.rs
+++ git-version/tests/version.rs
@@ -2,12 +2,12 @@ use git_version::git_describe;
 
 #[test]
 fn git_describe_is_right() {
-    let vec = std::process::Command::new("git")
-            .args(&["describe", "--always", "--dirty=-modified"])
-            .output()
-        .expect("failed to execute git").stdout;
-    let name = std::str::from_utf8(&vec[..vec.len()-1]).expect("non-utf8 error?!");
-    println!("name = {}", name);
-    println!("GIT_VERSION = {}", git_describe!(--dirty=-modified));
-    assert_eq!(git_describe!(--dirty=-modified), name);
+	let vec = std::process::Command::new("git")
+		.args(&["describe", "--always", "--dirty=-modified"])
+		.output()
+		.expect("failed to execute git").stdout;
+	let name = std::str::from_utf8(&vec[..vec.len()-1]).expect("non-utf8 error?!");
+	println!("name = {}", name);
+	println!("GIT_VERSION = {}", git_describe!(--dirty=-modified));
+	assert_eq!(git_describe!(--dirty=-modified), name);
 }
