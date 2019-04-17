6ba30f13dfe426612636d7a1f2e9e50620edcc72
Maarten de Vries <maarten@de-vri.es>
2019-04-17 19:46:27 +0200

    Replace .expect() with syn::Result for better error reporting.

--- git-version-macro/src/lib.rs
+++ git-version-macro/src/lib.rs
@@ -5,32 +5,22 @@ use proc_macro_hack::proc_macro_hack;
 use quote::quote;
 use syn::parse_macro_input;
 
-use git_version_impl::{version_cwd, describe_cwd, git_dir_cwd};
+use git_version_impl::{describe_cwd, git_dir_cwd, VERSION_ARGS};
 
-/// Create a token stream representing dependencies on the git state.
-fn git_dependencies() -> proc_macro2::TokenStream {
-	let git_dir = git_dir_cwd().expect("failed to determine .git directory");
-
-	let head = git_dir.join("logs/HEAD").canonicalize().expect("failed to canonicalize path to .git/logs/HEAD");
-	let head = head.to_str().expect("invalid UTF-8 in path to .git/logs/HEAD");
-
-	quote!{
-		include_bytes!(#head);
-	}
+macro_rules! error {
+	($($args:tt)*) => { syn::Error::new(proc_macro2::Span::call_site(), format!($($args)*)) };
 }
 
-#[proc_macro]
-pub fn declare(input: TokenStream) -> TokenStream {
+/// Create a token stream representing dependencies on the git state.
+fn git_dependencies() -> syn::Result<proc_macro2::TokenStream> {
+	let git_dir = git_dir_cwd().map_err(|e| error!("failed to determine .git directory: {}", e))?;
 
-	let identifier = proc_macro2::TokenStream::from(input);
+	let head = git_dir.join("logs/HEAD").canonicalize().map_err(|e| error!("failed to canonicalize path to .git/logs/HEAD: {}", e))?;
+	let head = head.to_str().ok_or_else(|| error!("invalid UTF-8 in path to .git/logs/HEAD"))?;
 
-	let version = version_cwd().expect("failed to determine git version");
-	let mut tokens = quote!{
-		const #identifier: &'static str = #version;
-	};
-
-	tokens.extend(git_dependencies());
-	tokens.into()
+	Ok(quote!{
+		include_bytes!(#head);
+	})
 }
 
 struct ArgList{
@@ -64,13 +54,12 @@ impl syn::parse::Parse for Nothing {
 pub fn git_describe(input: TokenStream) -> TokenStream {
 	let args : Vec<_> = parse_macro_input!(input as ArgList).args.iter().map(|x| x.value()).collect();
 
-	let version      = describe_cwd(&args).expect("failed to run `git describe`");
-	let dependencies = git_dependencies();
+	let tokens = match git_describe_impl(args) {
+		Ok(x) => x,
+		Err(e) => e.to_compile_error(),
+	};
 
-	quote!({
-		#dependencies;
-		#version
-	}).into()
+	TokenStream::from(tokens)
 }
 
 /// Get the git version for the source code.
@@ -87,11 +76,23 @@ pub fn git_describe(input: TokenStream) -> TokenStream {
 pub fn git_version(input: TokenStream) -> TokenStream {
 	parse_macro_input!(input as Nothing);
 
-	let version      = version_cwd().expect("failed to run `git describe`");
-	let dependencies = git_dependencies();
+	let tokens = match git_describe_impl(&VERSION_ARGS) {
+		Ok(x) => x,
+		Err(e) => e.to_compile_error(),
+	};
 
-	quote!({
+	TokenStream::from(tokens)
+}
+
+fn git_describe_impl<I, S>(args: I) -> syn::Result<proc_macro2::TokenStream> where
+	I: IntoIterator<Item = S>,
+	S: AsRef<std::ffi::OsStr>,
+{
+	let version      = describe_cwd(args).map_err(|e| error!("{}", e))?;
+	let dependencies = git_dependencies()?;
+
+	Ok(quote!({
 		#dependencies;
 		#version
-	}).into()
+	}))
 }
