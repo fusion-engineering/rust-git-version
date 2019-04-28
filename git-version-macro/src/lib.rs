a186395d02b6071fd051d09146e6fd398e514526
Maarten de Vries <maarten@de-vri.es>
2019-04-28 14:28:27 +0200

    Add index to git dependencies.

--- git-version-macro/src/lib.rs
+++ git-version-macro/src/lib.rs
@@ -1,25 +1,63 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use proc_macro2::{Span, TokenStream as TokenStream2};
 use proc_macro_hack::proc_macro_hack;
 use quote::quote;
 use syn::parse_macro_input;
+use std::path::Path;
 
 use git_version_impl::{describe_cwd, git_dir_cwd, VERSION_ARGS};
 
 macro_rules! error {
-	($($args:tt)*) => { syn::Error::new(proc_macro2::Span::call_site(), format!($($args)*)) };
+	($($args:tt)*) => { syn::Error::new(Span::call_site(), format!($($args)*)) };
+}
+
+macro_rules! warning {
+	($($args:tt)*) => { warning(Span::call_site(), format!($($args)*)) };
+}
+
+#[cfg(feature = "nightly")]
+fn warning(span: Span, warning: impl std::fmt::Display) -> TokenStream2 {
+	span.unwrap().warning(warning.to_string())
+}
+
+#[cfg(not(feature = "nightly"))]
+fn warning(_span: Span, warning: impl std::fmt::Display) -> TokenStream2 {
+	eprintln!("{}", warning);
+	TokenStream2::new()
+}
+
+/// Canonicalize the path to a file inside the git folder.
+fn canonicalize_git_path(git_dir: impl AsRef<Path>, file: impl AsRef<Path>) -> syn::Result<String> {
+	let git_dir = git_dir.as_ref();
+	let file    = file.as_ref();
+
+	let path = git_dir.join(file);
+	let path = path.canonicalize().map_err(|e| error!("failed to canonicalize {}: {}", path.display(), e))?;
+	let path = path.to_str().ok_or_else(|| error!("invalid UTF-8 in path to {}", file.display()))?;
+
+	Ok(String::from(path))
 }
 
 /// Create a token stream representing dependencies on the git state.
-fn git_dependencies() -> syn::Result<proc_macro2::TokenStream> {
+fn git_dependencies() -> syn::Result<TokenStream2> {
 	let git_dir = git_dir_cwd().map_err(|e| error!("failed to determine .git directory: {}", e))?;
 
-	let head = git_dir.join("logs/HEAD").canonicalize().map_err(|e| error!("failed to canonicalize path to .git/logs/HEAD: {}", e))?;
-	let head = head.to_str().ok_or_else(|| error!("invalid UTF-8 in path to .git/logs/HEAD"))?;
+	let head  = canonicalize_git_path(&git_dir, "logs/HEAD");
+	let index = canonicalize_git_path(&git_dir, "index");
+
+	let warning = head.as_ref().err().or(index.as_ref().err()).map(|error|
+		warning!("Failed to add dependencies on the git state: {}. The crate may not rebuild if the git state changes.", error)
+	);
+
+	let head  =  head.ok().map(|x| quote! { include_bytes!(#x); });
+	let index = index.ok().map(|x| quote! { include_bytes!(#x); });
 
 	Ok(quote!{
-		include_bytes!(#head);
+		#warning
+		#head
+		#index
 	})
 }
 
@@ -84,7 +122,7 @@ pub fn git_version(input: TokenStream) -> TokenStream {
 	TokenStream::from(tokens)
 }
 
-fn git_describe_impl<I, S>(args: I) -> syn::Result<proc_macro2::TokenStream> where
+fn git_describe_impl<I, S>(args: I) -> syn::Result<TokenStream2> where
 	I: IntoIterator<Item = S>,
 	S: AsRef<std::ffi::OsStr>,
 {
