8ce51b394cb4ab8eb0105740b12755a8fb13eb01
David Roundy <roundyd@physics.oregonstate.edu>
2019-11-27 05:40:48 -0800

    fix bug pointed out by m-ou-se

--- git-version-macro/src/lib.rs
+++ git-version-macro/src/lib.rs
@@ -104,11 +104,10 @@ pub fn git_describe(input: TokenStream) -> TokenStream {
 pub fn git_describe_or_cargo_version(input: TokenStream) -> TokenStream {
 	let args: Vec<_> = parse_macro_input!(input as ArgList).args.iter().map(|x| x.value()).collect();
 
-  let cargo_version = concat!(env!("CARGO_PKG_VERSION"), "-cargo");
 	let tokens = match git_describe_impl(args) {
 		Ok(x) => x,
 		Err(_) => quote!{
-      #cargo_version
+      concat!(env!("CARGO_PKG_VERSION"), "-cargo");
     }
 	};
 
@@ -131,11 +130,10 @@ pub fn git_version(input: TokenStream) -> TokenStream {
 pub fn git_or_cargo_version(input: TokenStream) -> TokenStream {
   parse_macro_input!(input as Nothing);
 
-  let cargo_version = concat!(env!("CARGO_PKG_VERSION"), "-cargo");
   let tokens = match git_describe_impl(&VERSION_ARGS) {
 	  Ok(x) => x,
 		Err(_) => quote!{
-      #cargo_version
+      concat!(env!("CARGO_PKG_VERSION"), "-cargo");
     }
 	};
 
